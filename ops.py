83745baffc7fb484edf09f8702592803ce011004
kimura <mateuskimura@gmail.com>
2020-05-12 14:57:18 -0300

    adapted to new op structture

--- ops.py
+++ ops.py
@@ -14,7 +14,7 @@ class Operators(object):
     def lookOpAvailable(self):
         # Look for operator available
         for op in self.operators:
-            if op.isAvailable:
+            if op.isAvailable():
                 return op
 
         # All operators are busy
