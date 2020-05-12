5ae9eecf800e36f0678055a3b73f3ea0cc082218
kimura <mateuskimura@gmail.com>
2020-05-12 16:09:24 -0300

    getOp function added / call is related to operator

--- call.py
+++ call.py
@@ -2,9 +2,16 @@ class Call(object):
     def __init__(self, ID):
         self.ID = ID
         self.status = None
+        self.op = None
 
     def setStatus(self, status):
         self.status = status
 
     def getStatus(self):
         return self.status
+
+    def setOp(self, op):
+        self.op = op
+
+    def getOp(self):
+        return self.op
