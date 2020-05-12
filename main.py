619e861658756701608da70b05656128d7e62bf6
kimura <mateuskimura@gmail.com>
2020-05-11 22:56:34 -0300

    Main skeleton

---
+++ main.py
@@ -0,0 +1,14 @@
+from client import Client
+from operator import Operator
+
+
+def main():
+    operatorA = Operator('A')
+    operatorB = Operator('B')
+    client1 = Client(1)
+    client2 = Client(2)
+
+
+
+if __name__ == '__main__':
+    main()
