dd22676e7e13676a4230ef378a8f29fbcf9f0bde
kimura <mateuskimura@gmail.com>
2020-05-12 14:55:00 -0300

    get status function added

--- call.py
+++ call.py
@@ -1,7 +1,10 @@
-class Call():
+class Call(object):
     def __init__(self, ID):
         self.ID = ID
         self.status = None
 
     def setStatus(self, status):
         self.status = status
+
+    def getStatus(self):
+        return self.status
