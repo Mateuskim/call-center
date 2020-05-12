2d232d28974b6ca1bb4d3ea5b43437b8d510153e
kimura <mateuskimura@gmail.com>
2020-05-12 15:06:04 -0300

    reject call function added

--- op.py
+++ op.py
@@ -27,6 +27,13 @@ class Operator(object):
                 return True
         return False
 
+    def rejectCall(self, op_id):
+        if self.ID == op_id:
+            if self.status == 'busy' and self.curCall.getStatus() == 'ringing':
+                self.curCall.setStatus('rejected')
+                return True
+        return False
+
     def isAvailable(self):
         if self.status == 'available':
             return True
