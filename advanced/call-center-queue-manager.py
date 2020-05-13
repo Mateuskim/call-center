d544c3ae5854529d7dc978f36f14aee03f0043ce
kimura <mateuskimura@gmail.com>
2020-05-12 21:53:51 -0300

    fix encode decode

--- advanced/call-center-queue-manager.py
+++ advanced/call-center-queue-manager.py
@@ -5,14 +5,14 @@ class Echo(protocol.Protocol):
     """This is just about the simplest possible protocol"""
 
     def dataReceived(self, data):
-        json_response = json.loads(data).decode("utf-8")
+        json_response = json.loads(data.decode("utf-8"))
         command = data["command"]
         id = data["id"]
         if command == "call":
             json_response["response"] = "Call " + id + "received"
 
 
-        answer = json.dump(json_response.encode("utf-8"))
+        answer = json.dump(json_response).encode("utf-8")
 
         "As soon as any data is received, write it back."
         self.transport.write(answer)
