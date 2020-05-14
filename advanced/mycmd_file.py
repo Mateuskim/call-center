d15c28a7a0c5dcf1526a38c5d9969eba2c7c23a3
kimura <mateuskimura@gmail.com>
2020-05-14 11:15:22 -0300

    file to test communication

---
+++ advanced/mycmd_file.py
@@ -0,0 +1,35 @@
+from sys import stdout
+from cmd import Cmd
+
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+
+class MyCmd(Cmd):
+    def __init__(self, client):
+        Cmd.__init__(self)
+        self.client = client
+
+    def default(self, line):
+        client.sendData(line)
+
+    def do_EOF(self, line):
+        if reactor.running:
+            reactor.stop()
+        stdout.write("\n")
+        return True
+
+
+class MyClient(Protocol):
+    def dataReceived(self, data):
+        stdout.write(data)
+
+    def sendData(self, data):
+        self.transport.write(data)
+        self.transport.write("\n")
+
+point = TCP4ClientEndpoint(reactor, "localhost", 6004)
+client = MyClient()
+connectProtocol(point, client)
+reactor.callInThread(MyCmd(client).cmdloop)
+reactor.run()
