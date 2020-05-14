8f2cf18680302f89fe88345147cd7f38ccc3db75
kimura <mateuskimura@gmail.com>
2020-05-14 15:45:08 -0300

    make myfactory

--- advanced/call-center-queue-manager.py
+++ advanced/call-center-queue-manager.py
@@ -1,15 +1,21 @@
 from twisted.internet import reactor
-from twisted.internet.protocol import ServerFactory
+from twisted.internet.protocol import Factory
+from twisted.internet.endpoints import TCP4ServerEndpoint
 from manager import Manager
-from myserver import MyServer
+from myserver import MyProtocol
 
+
+class MyFactory(Factory):
+    def __init__(self, manager):
+        self.manager = manager
+
+    def buildProtocol(self, addr):
+        return MyProtocol(manager=self.manager)
 def main():
-    
+
     manager = Manager()
-    """This runs the protocol on port 5678"""
-    factory = ServerFactory()
-    factory.protocol = MyServer(manager)
-    reactor.listenTCP(5678, factory)
+    endpoint = TCP4ServerEndpoint(reactor, 5678)
+    endpoint.listen(MyFactory(manager))
     reactor.run()
 
 # this only runs if the module was *not* imported
