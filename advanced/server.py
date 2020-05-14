af568c3d64f6b9b31bcd8a3acaf4006599f37e55
kimura <mateuskimura@gmail.com>
2020-05-14 14:28:39 -0300

    server class executing and returning a message in a string

--- advanced/server.py
+++ advanced/server.py
@@ -18,17 +18,23 @@ class Server:
 
     def execute_Command(self, command):
         if command == "call 1":
-            self.call(1)
+            print(self.call('1'))
+            print(self.reject('A'))
+
+            print(self.hangup('1'))
 
     # ------------------ Client commands -----------------------
 
     def call(self, call_id):
+        answer_message = ''
+
 
         #If the call input is correct
         if call_id != '':
 
             call = Call(call_id)
-            print("Call " + str(call_id) + " received")
+            # print("Call " + call_id + " received\n")
+            answer_message += "Call " + call_id + " received\n"
 
             #Adding call to the list of calls which are happening
             self.online_calls_list.addCall(call)
@@ -42,18 +48,23 @@ class Server:
                 op = self.searchOperator("available")
                 if op is not None:
 
+                    answer_message += "Call " + call_id + " ringing for operator " + op.ID + "\n"
+
                     #Allocate call to operator
-                    print("Call " + str(call_id) + " ringing for operator " + op.ID)
+                    # print("Call " + str(call_id) + " ringing for operator " + op.ID)
                     self.operators.setCall(op.ID, call)
                     go_queue = False
 
             #If is going to queue
             if go_queue:
-                print("Call " + call_id + " waiting in queue")
+                # print("Call " + call_id + " waiting in queue")
+                answer_message += "Call " + call_id + " waiting in queue\n"
                 call.setStatus("waiting")
                 self.call_queue.enqueue(call)
         else:
-            print('Must specify a call id ( Call <call_id>)')
+            answer_message = 'Must specify a call id ( Call <call_id>)'
+
+        return answer_message
 
 
 
@@ -61,32 +72,44 @@ class Server:
 
     def answer(self, op_id):
 
+        answer_message = ''
         #Search for operator and answer his curCall
-        call = self.searchOperator("answer", op_id)
+        call = self.searchOperator("searchCall", op_id)
 
         # If operator <op_id> has a call ringing
         if call is not None:
-            print("Call " + call.ID + " answered by operator " + op_id)
+            answer_message = "Call " + call.ID + " answered by operator " + op_id + "\n"
 
+        return answer_message
 
     def reject(self, op_id):
+
+        answer_message = ''
         call = self.searchOperator("reject", op_id)
 
 
         # If operator <op_id> has a call ringing
         if call is not None:
-            print("Call " + call.ID + " rejected by operator " + op_id)
+
+            # print("Call " + call.ID + " rejected by operator " + op_id)
+            answer_message += "Call " + call.ID + " rejected by operator " + op_id + "\n"
 
             #While don't find a new operator or don't happen hangup command
             while call.getStatus() != 'ringing' and call.getStatus() != 'ended':
                 op = self.searchOperator("available")
                 if op is not None:
+
                     # Allocate call to operator
-                    print("Call " + call.ID + " ringing for operator " + op.ID)
+                    # print("Call " + call.ID + " ringing for operator " + op.ID)
+                    answer_message += "Call " + call.ID + " ringing for operator " + op.ID + "\n"
                     self.operators.setCall(op.ID, call)
+
+        return answer_message
 
 
     def hangup(self, call_id):
+
+        answer_message = ''
         #Search for the call
         call = self.online_calls_list.searchCall(call_id)
         if call is not None:
@@ -97,7 +120,8 @@ class Server:
                 #Set call status to ended and delete from the list of online calls
                 call.setStatus('ended')
                 self.online_calls_list.endCall(call)
-                print("Call " + call_id + " missed")
+                # print("Call " + call_id + " missed")
+                answer_message += "Call " + call_id + " missed\n"
 
             #If call is answered
             elif call.getStatus() == 'answered' or call.getStatus() == 'ringing':
@@ -107,9 +131,11 @@ class Server:
                 # answered and ringing is the same
 
                 if call.getStatus() == 'answered':
-                    print("Call " + call.ID + " finished and operator " + op.ID + " available")
+                    # print("Call " + call.ID + " finished and operator " + op.ID + " available")
+                    answer_message += "Call " + call.ID + " finished and operator " + op.ID + " available\n"
                 else:
                     print("Call " + call.ID + " missed")
+                    answer_message += "Call " + call.ID + " missed\n"
 
                 call.setStatus('ended')
                 #unlink operator and call and delete from online calls
@@ -128,9 +154,9 @@ class Server:
                     if new_call.status != 'ended':
 
                         self.operators.setCall(op.ID, new_call)
-
+                        answer_message += "Call " + new_call.ID + " ringing for operator " + op.ID + '\n'
                         print("Call " + new_call.ID + " ringing for operator " + op.ID)
-
+        return answer_message
 
 
     def searchOperator(self, command, op_id = None):
@@ -142,7 +168,7 @@ class Server:
 
         #For answer command the operator needs to be busy and with a
         #call ringing
-        elif command == 'answer':
+        elif command == 'searchCall':
             return self.operators.lookOpAnswer(op_id)
 
         # For reject command the operator needs to be busy and with a
