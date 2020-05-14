3b64a1394ddb8cec72fec8084bc4d7e5524794e5
kimura <mateuskimura@gmail.com>
2020-05-14 15:00:11 -0300

    separate call-center-manager myServer class and message read threatment

--- advanced/call-center-queue-manager.py
+++ advanced/call-center-queue-manager.py
@@ -1,41 +1,15 @@
-from twisted.internet import reactor, protocol
 import json
-
-def translateCommand(str_json):
-    json_response = json.loads(str_json.decode("utf-8"))
-    command = json_response["command"]
-    id = json_response["id"]
-    command_string = command + " " + id
-    return command_string
-
-
-def executeCommand(command):
-    return command + "\n"
-
-
-def createJson(answer):
-    answer_json = {}
-    answer_json["response"] = answer
-    return json.dumps(answer_json).encode("utf-8")
-
-
-class Echo(protocol.Protocol):
-    """This is just about the simplest possible protocol"""
-
-
-    def dataReceived(self, data):
-        command = translateCommand(data)
-        answer_string = executeCommand(command)
-        answer_json = createJson(answer_string)
-        self.transport.write(answer_json)
-
-
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ServerEndpoint, connectProtocol
+from manager import Manager
+from myserver import MyServer
 
 def main():
-    """This runs the protocol on port 5678"""
-    factory = protocol.ServerFactory()
-    factory.protocol = Echo
-    reactor.listenTCP(5678, factory)
+    """Declare Server class"""
+    myManager = Manager()
+    point = TCP4ServerEndpoint(reactor, 5678)
+    server = MyServer(myManager)
+    connectProtocol(point, server)
     reactor.run()
 
 
