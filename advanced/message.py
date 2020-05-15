bda45613464d8b56e2948e6dfa13cca6bc62507c
kimura <mateuskimura@gmail.com>
2020-05-15 13:10:08 -0300

    add again message/ deleted before

---
+++ advanced/message.py
@@ -0,0 +1,37 @@
+# Functions to threat message reading
+
+import json
+
+
+def createResponse(answer):
+    answer_json = {}
+    answer_json["response"] = answer
+    return json.dumps(answer_json).encode("utf-8")
+
+
+def createCommand(command):
+    json_command = {}
+    json_command["command"] = command[:-2]
+    json_command["id"] = command[-1:]
+    json_command["response"] = "mensagem default"
+    return json_command
+
+
+def packJson(json_message):
+    return json.dumps(json_message).encode("utf-8")
+
+
+def translateMessage(message_answer):
+    return json.loads(message_answer.decode("utf-8"))
+
+
+def getCommand(json_file):
+    return json_file["command"]
+
+
+def getID(json_file):
+    return json_file["id"]
+
+
+def getAnswer(message_json):
+    return message_json["response"]
