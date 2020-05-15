0c59d2942f219e483979ba7926fc5f3cadc247ca
kimura <mateuskimura@gmail.com>
2020-05-15 12:46:39 -0300

    protocols from server and client

---
+++ advanced/myclient.py
@@ -0,0 +1,19 @@
+from twisted.internet.protocol import Protocol
+from sys import stdout
+from message import *
+
+class MyClient(Protocol):
+
+    def connectionMade(self):
+        print("Connection Made")
+
+    def dataReceived(self, data):
+        answer_json = translateMessage(data)
+        answer = getAnswer(answer_json)
+        print("========== ANSWER FROM SERVER ==============")
+        print(answer, end='')
+        print("============================================\n")
+        stdout.flush()
+
+    def sendData(self, data):
+        self.transport.write(data)
--- advanced/myserver.py
+++ advanced/myserver.py
@@ -1,7 +1,7 @@
 from twisted.internet.protocol import Protocol
+from sys import stdout
 
 from message import *
-
 
 class MyProtocol(Protocol):
     """This is just about the simplest possible protocol"""
@@ -9,9 +9,12 @@ class MyProtocol(Protocol):
     def __init__(self, manager):
         self.manager = manager
 
+    def connectionMade(self):
+        stdout.write("Connection made")
+
 
     def dataReceived(self, data):
-        command = translateCommand(data)
+        command = translateMessage(data)
         answer_string = self.manager.execute_Command(command)
-        answer_json = createJson(answer_string)
+        answer_json = createResponse(answer_string)
         self.transport.write(answer_json)
